05687bf15c95b5f2064367b9858ae0af4bd10bb7
lilleyz7 <lilley5>
2024-10-18 20:00:03 -0700

    update settings and cors

--- authentication/urls.py
+++ authentication/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('register', register, name='register')
+    path('register/', register, name='register')
 ]
--- breweryapi/settings.py
+++ breweryapi/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1424ckcyknb7i)g+jgc(^n3qh*h6nn+1wz7)a_&((_x-@jy$_h'
+SECRET_KEY = 'dsfa9678fsdga789tfsda98g71424ckcyknb7i)g+jgc(^n3qh*h6nn+1wz7)a_&((_x-@jy$_h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    "corsheaders",
     'api',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -49,7 +50,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -58,14 +60,12 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
 
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
-
 }
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:5173",
+]
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
